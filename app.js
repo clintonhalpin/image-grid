0f169bc0014bf5dbc716f4d37658a761cb783288
clintonhalpin <clintonhalpin@gmail.com>
2018-03-06 10:54:41 -0500

    Make selected photo an object

--- app.js
+++ app.js
@@ -2,20 +2,39 @@
  * UI Components
  */
 const app = document.getElementById("app");
-// const url = 'https://api.unsplash.com/photos?page=1&per_page=20&client_id=2af528ddceed2cfff8cf179322cd972ff6c54e772e471193cdbfc628d801dacd'
-const url = 'https://api.unsplash.com/photos?page=3&per_page=24&client_id=ec4779e6804bf4e4c72ac5f5d70a81480712b24f2ecfe46494169d9b74ee9f83'
+
+/**
+ * Kick off the app
+ */
+document.addEventListener("DOMContentLoaded", () => {
+  let params = {
+    page: 4,
+    per_page: 24,
+    client_id:
+      "ec4779e6804bf4e4c72ac5f5d70a81480712b24f2ecfe46494169d9b74ee9f83"
+  };
+  let url = "https://api.unsplash.com/photos";
+  fetchData(url + queryString(params), "photos").then(res => {
+    state = setState({ photos: res });
+    render(state);
+  });
+});
 
 /**
  * State
  */
 let state = {
-	selectedPhoto: false,
-	photos: []
+  selectedPhoto: false,
+  photos: []
 };
 
+/**
+ * [setState description]
+ * @param {[type]} nextState [description]
+ */
 function setState(nextState) {
-	state = Object.assign(state, nextState)
-	return state
+  state = Object.assign(state, nextState);
+  return state;
 }
 
 /**
@@ -24,27 +43,47 @@ function setState(nextState) {
  * @return {[type]}          [description]
  */
 function fetchData(endpoint) {
-	const cache = localStorage.getItem(endpoint)
-	const p = new Promise((resolve, reject) => {
-		const options = { 
-			method: 'GET',
-		};
+  const cache = localStorage.getItem(endpoint);
+  const p = new Promise((resolve, reject) => {
+    const options = {
+      method: "GET"
+    };
+    if (cache) {
+      resolve(JSON.parse(cache));
+    } else {
+      fetch(endpoint, options)
+        .then(response => {
+          if (200 !== response.status) {
+            reject("Error");
+          }
+          return response.json();
+        })
+        .then(json => {
+          localStorage.setItem(endpoint, JSON.stringify(json));
+          resolve(json);
+        });
+    }
+  });
+  return p;
+}
 
-		if( cache ) {
-			resolve(JSON.parse(cache))
-		} else {
-			fetch(endpoint, options).then((response) => {
-			  if( 200 !== response.status ) {
-			    reject('Error')
-			  }
-			  return response.json()
-			}).then((json) => {
-			  localStorage.setItem(endpoint, JSON.stringify(json))
-			 	resolve(json)
-			})
+/**
+ * [findPhotoById description]
+ * @param  {[type]} id    [description]
+ * @param  {[type]} state [description]
+ * @return {[type]}       [description]
+ */
+function findPhotoById( id, state ) {
+	let match = false;
+	state.photos.map((photo, idx) => {
+		if( id === photo.id ) {
+			match = {
+				idx: idx,
+				photo: photo
+			}
 		}
 	})
-	return p
+	return match
 }
 
 /**
@@ -56,12 +95,13 @@ function toggleClass(el, className) {
   if (el.classList) {
     el.classList.toggle(className);
   } else {
-    var classes = el.className.split(" ");
-    var existingIndex = classes.indexOf(className);
-
-    if (existingIndex >= 0) classes.splice(existingIndex, 1);
-    else classes.push(className);
-
+    let classes = el.className.split(" ");
+    let existingIndex = classes.indexOf(className);
+    if (existingIndex >= 0) {
+    	classes.splice(existingIndex, 1);
+    } else {
+    	classes.push(className);
+    }
     el.className = classes.join(" ");
   }
 }
@@ -72,38 +112,45 @@ function toggleClass(el, className) {
  * @return {[type]}       [description]
  */
 function renderPhoto(photo) {
-	return `<img id="${ photo.id }" class="image" src="${ photo.urls.regular }" />`
+  return `<img id="${photo.id}" class="image" src="${photo.urls.regular}" />`;
 }
-
 
 function renderModalControls(state) {
-	return `<div class="modal__controls mb2 right-align">
+  return `<div class="modal__controls mb2 right-align">
 						<a id="close" class="btn btn-outline rounded black mr2 left" href="#">Back</a>
 						<a id="prev" class="btn btn-outline rounded black mr2" href="#">Prev</a>
-						<a id="next" class="btn btn-outline rounded black " href="#">Next</a>
-					</div>`
+						<a id="next" class="btn btn-outline rounded black " href="#">Next</a>			
+					</div>`;
 }
 
-function cyclePhoto(state, direction = 'next') {
-	let { selectedPhoto, photos } = state;
-	let selectedPhotoIdx = false;
-	let data = photos.slice()
+function cyclePhoto(state, direction = "next") {
+  let { selectedPhoto, photos } = state;
+  let currentPhoto = findPhotoById(selectedPhoto.id, state);
+  let nextIndex = 'next' === direction ? currentPhoto.idx + 1 : currentPhoto.idx - 1;
+  /**
+   * @note, This logic seems to work, w/o conditional check for direction. 
+   * Might need a bit of debugging
+   * On First Photo, hit prev
+   * On last photo, hit next
+   */
+  if( nextIndex >= photos.length ) {
+  	nextIndex = 0;
+  }
+  if( -1 === nextIndex) {
+  	nextIndex = photos.length - 1;
+  }
+  setState({ selectedPhoto : state.photos[ nextIndex ] })
+}
 
-	if( 'prev' === direction ) {
-		data = data.reverse()
-	}
-
-	data.map((photo, idx) => {
-		if( selectedPhoto === photo.id ) {
-			selectedPhotoIdx = idx
-		}
-	})
-
-	if( selectedPhotoIdx >= 0 ) {
-		let next = photos[ selectedPhotoIdx + 1 ];
-		next = next ? next.id : photos[0].id
-		setState({ selectedPhoto : next })
-	}
+/**
+ * [getEndpoint description]
+ * @return {[type]} [description]
+ */
+function queryString(params) {
+  let query = Object.keys(params)
+    .map(key => key + "=" + params[key])
+    .join("&");
+  return "?" + query;
 }
 
 /**
@@ -111,22 +158,17 @@ function cyclePhoto(state, direction = 'next') {
  * @param  {[type]} state [description]
  * @return {[type]}       [description]
  */
-function renderModal( state ) {
-	const { selectedPhoto, photos } = state;
-	let currentPhoto = false;
-	photos.map(photo => {
-		if( selectedPhoto === photo.id ) {
-			currentPhoto = photo
-		}
-	})
-	return [
-		`<div class="modal ${ currentPhoto ? 'modal--active' : ''}">`, 
-		'<div class="modal__inner p2 bg-white rounded">',
-		renderModalControls(), 
-		currentPhoto ? renderPhoto(currentPhoto) : '', 
-		'</div>',
-		'</div>'
-	].join('')
+function renderModal(state) {
+  const { selectedPhoto, photos } = state;
+  return [
+    `<div class="modal ${selectedPhoto ? "modal--active" : ""}">`,
+    '<div id="overlay" class="modal__overlay"></div>',
+    '<div class="modal__inner p2 bg-white rounded">',
+    renderModalControls(),
+    selectedPhoto ? renderPhoto(selectedPhoto) : "",
+    "</div>",
+    "</div>"
+  ].join("");
 }
 
 /**
@@ -134,9 +176,17 @@ function renderModal( state ) {
  * @return {[type]} [description]
  */
 function render(state) {
-  let photos = state.photos.map(photo => `<div id="${photo.id}" class="image image-col col col-6 md-col md-col-3" style="background-image:url(${photo.urls.regular})"></div>`)
-  app.innerHTML = [ renderModal(state), ...photos ].join('');
-  addListeners()
+  let classNames = "image image-col col col-6 md-col md-col-3";
+  let photos = state.photos.map(
+    photo =>
+      `<div id="${
+        photo.id
+      }" class="${classNames}" style="background-image:url(${
+        photo.urls.regular
+      })"></div>`
+  );
+  app.innerHTML = [renderModal(state), ...photos].join("");
+  addListeners();
 }
 
 /**
@@ -144,44 +194,58 @@ function render(state) {
  * @return {[type]} [description]
  */
 function addListeners() {
-	
-	const images = document.querySelectorAll('.image');
+  const images = document.querySelectorAll(".image");
 
-	for (var i = 0; i < images.length; i++) {
-		images[i].addEventListener('click', (e) => {
-			toggleClass(document.body, 'no-scroll')
-			setState({ selectedPhoto: e.target.id })
-			render(state)
-		})
-	}
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", e => {
+      toggleClass(document.body, "no-scroll");
+      let photo = findPhotoById(e.target.id, state);
+      setState({ selectedPhoto: photo.photo });
+      render(state);
+    });
+  }
 
-	document.getElementById("close").addEventListener('click', e => {
-		e.preventDefault()
-		toggleClass(document.body, 'no-scroll')
-		setState({ selectedPhoto: false })
-		render(state)
-	})
+	document.getElementById("overlay").addEventListener("click", close);
+  document.getElementById("close").addEventListener("click", close);
+  document.getElementById("next").addEventListener("click", next);
+  document.getElementById("prev").addEventListener("click", prev);
 
-	document.getElementById("next").addEventListener('click', e => {
-		e.preventDefault()
-		cyclePhoto(state, 'next')
-		render(state)
-	})
+  document.body.addEventListener("keydown", e => {
+    switch (e.keyCode) {
+      case 39:
+        next(e);
+        break;
+      case 37:
+        prev(e);
+        break;
+    }
+  });
+}
 
-	document.getElementById("prev").addEventListener('click', e => {
-		e.preventDefault()
-		cyclePhoto(state, "prev")
-		render(state)
-	})
-
+function close(e) {
+	e.preventDefault();
+	toggleClass(document.body, "no-scroll");
+	setState({ selectedPhoto: false });
+	render(state);
 }
 
 /**
- * Fetch the Initial data, and render the app
+ * [next description]
+ * @param  {[type]}   e [description]
+ * @return {Function}   [description]
  */
-document.addEventListener("DOMContentLoaded", () => {
-	fetchData(url, 'photos').then((res) => {
-  	state = Object.assign(state, { photos: res })
-  	render(state)
-  })
-});
+function next(e) {
+  e.preventDefault();
+  cyclePhoto(state, "next");
+  render(state);
+}
+/**
+ * [prev description]
+ * @param  {[type]} e [description]
+ * @return {[type]}   [description]
+ */
+function prev(e) {
+  e.preventDefault();
+  cyclePhoto(state, "prev");
+  render(state);
+}
