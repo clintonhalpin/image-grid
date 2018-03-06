7e82d71185ef06fdf880f595a5066dfc8a85867b
clintonhalpin <clintonhalpin@gmail.com>
2018-03-06 11:59:24 -0500

    Refactor things

--- app.js
+++ app.js
@@ -2,34 +2,61 @@
  * Selectors
  */
 const app = document.getElementById("app");
-/**
- * Kick off the app
- */
-document.addEventListener("DOMContentLoaded", () => {
-  let params = {
-    page: 4,
-    per_page: 24,
-    client_id:
-      "ec4779e6804bf4e4c72ac5f5d70a81480712b24f2ecfe46494169d9b74ee9f83"
-  };
-  let url = "https://api.unsplash.com/photos";
-  fetchData(url + queryString(params), "photos").then(res => {
-    state = setState({ photos: res });
-    render(state);
-  });
-});
+
 
 /**
  * State
  */
-let state = {
+let state = {};
+let initialState = {
   selectedPhoto: false,
   photos: []
 };
+let API_BASE = "https://api.unsplash.com/photos";
+
+/**
+ * Boot the App
+ */
+document.addEventListener("DOMContentLoaded", boot);
+
+/**
+ * [boot Start the application]
+ * @return {side-effect} [description]
+ */
+function boot() {
+	setState(initialState)
+	render(state);
+	const params = {
+    page: 4,
+    per_page: 24,
+    client_id:
+      "ec4779e6804bf4e4c72ac5f5d70a81480712b24f2ecfe46494169d9b74ee9f83"
+	};
+	fetchData(API_BASE + queryString(params)).then(response => {
+		setState(initialState);
+		setState({ photos : response });
+		render(state);
+	})
+}
+
+/**
+ * Utility functions
+ */
+
+/**
+ * [queryString description]
+ * @return {[type]} [description]
+ */
+function queryString(params) {
+  let query = Object.keys(params)
+    .map(key => key + "=" + params[key])
+    .join("&");
+  return "?" + query;
+}
 
 /**
  * [setState description]
- * @param {[type]} nextState [description]
+ * @param {object} nextState [description]
  */
 function setState(nextState) {
   state = Object.assign(state, nextState);
@@ -37,52 +64,14 @@ function setState(nextState) {
 }
 
 /**
- * [fetchData description]
- * @param  {[type]} endpoint [description]
- * @return {[type]}          [description]
+ * [queryString build a query string from object]
+ * @return {string} [description]
  */
-function fetchData(endpoint) {
-  const cache = localStorage.getItem(endpoint);
-  const p = new Promise((resolve, reject) => {
-    const options = {
-      method: "GET"
-    };
-    if (cache) {
-      resolve(JSON.parse(cache));
-    } else {
-      fetch(endpoint, options)
-        .then(response => {
-          if (200 !== response.status) {
-            reject("Error");
-          }
-          return response.json();
-        })
-        .then(json => {
-          localStorage.setItem(endpoint, JSON.stringify(json));
-          resolve(json);
-        });
-    }
-  });
-  return p;
-}
-
-/**
- * [findPhotoById description]
- * @param  {[type]} id    [description]
- * @param  {[type]} state [description]
- * @return {[type]}       [description]
- */
-function findPhotoById( id, state ) {
-	let match = false;
-	state.photos.map((photo, idx) => {
-		if( id === photo.id ) {
-			match = {
-				idx: idx,
-				photo: photo
-			}
-		}
-	})
-	return match
+function queryString(params) {
+  let query = Object.keys(params)
+    .map(key => key + "=" + params[key])
+    .join("&");
+  return "?" + query;
 }
 
 /**
@@ -106,36 +95,71 @@ function toggleClass(el, className) {
 }
 
 /**
- * [renderPhoto description]
- * @param  {[type]} photo [description]
- * @return {[type]}       [description]
+ * [findById description]
+ * @param  {any} id  [In the case of unsplash we use strings, but it could be an int]
+ * @param  {array} arr [description]
+ * @return {false|object}     [description]
  */
-function renderPhoto(photo) {
-  return `<img id="${photo.id}" class="image" src="${photo.urls.regular}" />`;
+function findById( id, arr ) {
+	let match = false;
+	arr.map((item, idx) => {
+		if( id === item.id ) {
+			match = {
+				idx,
+				item
+			}
+		}
+	})
+	return match
 }
 
 /**
- * [renderModalControls description]
- * @param  {[type]} state [description]
- * @return {[type]}       [description]
+ * Core functionality
  */
-function renderModalControls(state) {
-  return `<div class="modal__controls mb2 right-align">
-						<a id="close" class="btn btn-outline rounded black mr2 left" href="#">Back</a>
-						<a id="prev" class="btn btn-outline rounded black mr2" href="#">Prev</a>
-						<a id="next" class="btn btn-outline rounded black " href="#">Next</a>			
-					</div>`;
+
+/**
+ * [fetchData Fetch data, and cache it locally]
+ * @param  {string} endpoint [description]
+ * @return {promise}          [description]
+ */
+function fetchData( endpoint ) {
+	const cache = localStorage.getItem(endpoint);
+	const p = new Promise((resolve, reject) => {
+	  const options = {
+	    method: "GET"
+	  };
+	  if (cache) {
+	    resolve(JSON.parse(cache));
+	  } else {
+	    fetch(endpoint, options)
+	      .then(response => {
+	        if (200 !== response.status) {
+	          reject("Error");
+	        }
+	        return response.json();
+	      })
+	      .then(json => {
+	        localStorage.setItem(endpoint, JSON.stringify(json));
+	        resolve(json);
+	      });
+	  }
+	});
+	return p;	
 }
 
-function cyclePhoto(state, direction = "next") {
+/**
+ * [changePhoto description]
+ * @param  {[type]} state     [description]
+ * @param  {[type]} direction [description]
+ * @return {[type]}           [description]
+ */
+function changePhoto(state, direction) {
   let { selectedPhoto, photos } = state;
-  let currentPhoto = findPhotoById(selectedPhoto.id, state);
+  let currentPhoto = findById(selectedPhoto.id, state.photos);
   let nextIndex = 'next' === direction ? currentPhoto.idx + 1 : currentPhoto.idx - 1;
   /**
    * @note, This logic seems to work, w/o conditional check for direction. 
    * Might need a bit of debugging
-   * On First Photo, hit prev
-   * On last photo, hit next
    */
   if( nextIndex >= photos.length ) {
   	nextIndex = 0;
@@ -143,80 +167,49 @@ function cyclePhoto(state, direction = "next") {
   if( -1 === nextIndex) {
   	nextIndex = photos.length - 1;
   }
-  setState({ selectedPhoto : state.photos[ nextIndex ] })
+  setState({ selectedPhoto : state.photos[ nextIndex ] })	
 }
 
 /**
- * [getEndpoint description]
- * @return {[type]} [description]
+ * [addEventListeners Once render completes, we will want to hook up event listeners]
  */
-function queryString(params) {
-  let query = Object.keys(params)
-    .map(key => key + "=" + params[key])
-    .join("&");
-  return "?" + query;
-}
+function addEventListeners(){
 
-/**
- * [renderModal description]
- * @param  {[type]} state [description]
- * @return {[type]}       [description]
- */
-function renderModal(state) {
-  const { selectedPhoto, photos } = state;
-  return [
-    `<div class="modal ${selectedPhoto ? "modal--active" : ""}">`,
-    '<div id="overlay" class="modal__overlay"></div>',
-    '<div class="modal__inner p2 bg-white rounded">',
-    renderModalControls(),
-    selectedPhoto ? renderPhoto(selectedPhoto) : "",
-    "</div>",
-    "</div>"
-  ].join("");
-}
-
-/**
- * [render description]
- * @return {[type]} [description]
- */
-function render(state) {
-  let classNames = "image image-col col col-6 md-col md-col-3";
-  let photos = state.photos.map(
-    photo =>
-      `<div id="${
-        photo.id
-      }" class="${classNames}" style="background-image:url(${
-        photo.urls.regular
-      })"></div>`
-  );
-  app.innerHTML = [renderModal(state), ...photos].join("");
-  addListeners();
-}
-
-/**
- * [addListeners description]
- * @return {[type]} [description]
- */
-function addListeners() {
   const images = document.querySelectorAll(".image");
+  if( images.length ) {
+	  for (var i = 0; i < images.length; i++) {
+	    images[i].addEventListener("click", e => {
+	      toggleClass(document.body, "no-scroll");
+	      setState({ selectedPhoto: findById(e.target.id, state.photos).item });
+	      render(state);
+	    });
+	  }
+	}
 
-  for (var i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", e => {
-      toggleClass(document.body, "no-scroll");
-      let photo = findPhotoById(e.target.id, state);
-      setState({ selectedPhoto: photo.photo });
-      render(state);
-    });
-  }
+  const overlay = document.getElementById("overlay");
+  const closeBtn = document.getElementById("close");
+  const nextBtn = document.getElementById("next");
+  const prevBtn = document.getElementById("prev");
 
-	document.getElementById("overlay").addEventListener("click", close);
-  document.getElementById("close").addEventListener("click", close);
-  document.getElementById("next").addEventListener("click", next);
-  document.getElementById("prev").addEventListener("click", prev);
+  if( overlay ) {
+		overlay.addEventListener("click", close);
+	}
+
+	if( closeBtn ) {
+		closeBtn.addEventListener("click", close);
+	}
+
+	if( nextBtn ) {
+		nextBtn.addEventListener("click", next);
+	}
+
+	if( prevBtn ) {
+		prevBtn.addEventListener("click", prev);	
+	}
 }
 
 /**
- * [close description]
+ * [next Change to the next photo]
  * @param  {[type]} e [description]
  * @return {[type]}   [description]
  */
@@ -228,22 +221,122 @@ function close(e) {
 }
 
 /**
- * [next description]
- * @param  {[type]}   e [description]
- * @return {Function}   [description]
+ * [next Change to the next photo]
+ * @param  {[type]} e [description]
+ * @return {[type]}   [description]
  */
 function next(e) {
   e.preventDefault();
-  cyclePhoto(state, "next");
+  changePhoto(state);
   render(state);
 }
+
 /**
- * [prev description]
+ * [prev, Previous Photo]
  * @param  {[type]} e [description]
  * @return {[type]}   [description]
  */
 function prev(e) {
   e.preventDefault();
-  cyclePhoto(state, "prev");
+  changePhoto(state, "prev");
   render(state);
+}
+
+/**
+ * [renderModalControls description]
+ * @param  {[type]} state [description]
+ * @return {[type]}       [description]
+ */
+function renderModalControls(state) {
+  return `
+		<div class="modal__controls mb2 right-align">
+			<a id="close" class="btn btn-outline rounded black mr2 left" href="#">Back</a>
+			<a id="prev" class="btn btn-outline rounded black mr2" href="#">Prev</a>
+			<a id="next" class="btn btn-outline rounded black " href="#">Next</a>			
+		</div>
+	`;
+}
+
+/**
+ * [renderModalPhoto description]
+ * @param  {[type]} state [description]
+ * @return {[type]}       [description]
+ */
+function renderModalPhoto(state) {
+	const { selectedPhoto } = state;
+	if( ! selectedPhoto ) {
+		return '';
+	}
+	console.log(selectedPhoto);
+	return `
+		<img id="${selectedPhoto.id}" class="image" src="${selectedPhoto.urls.regular}" />
+	`
+}
+
+/**
+ * [renderModal renders the modal component]
+ * @param  {object} state [description]
+ * @return {string}       [description]
+ */
+function renderModal(state){
+	const { selectedPhoto } = state;
+	return `
+		<div class="modal ${ selectedPhoto ? "modal--active" : "" }">
+			<div id="overlay" class="modal__overlay"></div>
+			<div class="modal__inner p2 bg-white rounded">
+				${ renderModalControls(state) }
+				${ renderModalPhoto(state) }
+			</div>
+		</div>
+	`;
+} 
+
+/**
+ * [renderPhotos description]
+ * @param  {object} state [description]
+ * @return {string}       [description]
+ */
+function renderPhotos(state){
+
+	if( ! state.photos) {
+		return '<h1 class="center">Loading...</h1>';
+	}
+
+	let classNames = "image image-col col col-6 md-col md-col-3";
+	let photos = state.photos.map(
+    photo =>
+      `<div id="${
+        photo.id
+      }" class="${classNames}" style="background-image:url(${
+        photo.urls.regular
+      })"></div>`
+  );
+	return photos.join('');
+}
+
+/**
+ * [renderHeader description]
+ * @param  {object} state [description]
+ * @return {string}       [description]
+ */
+function renderHeader(state){
+	return `
+		<div class="py3 px2">
+			<h1 class="black line-height-1 m0">all files ${state.photos ? '(' + state.photos.length + ')' : ''}</h1>
+		</div>
+	`
+}
+
+/**
+ * [render Main render function, similar to react]
+ * @param  {object} state [the application state]
+ * @return {side-effect}  [Could return a string but for simplicity just append to app]
+ */
+function render(state) {
+	app.innerHTML =  [
+		renderHeader(state),
+		renderModal(state),
+		renderPhotos(state)
+	].join('');
+	addEventListeners();
 }
